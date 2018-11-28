70ec39d399c869adf3543b5b3ebd8c48b1fbd01f
Johanna <johannarklay@gmail.com>
2018-11-28 16:05:25 -0600

    re-do website to portfolio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Headlines from './Details/Headlines.jsx';
+import Projects from './Details/Projects';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            <h4>Welcome to </h4>
+            <h1>Johanna's Portfolio</h1>
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <Headlines />
+        <Projects/>
       </div>
+     
     );
   }
 }
